30ca36d05dbaca25ad7c811ae7abd18fc4418b9e
Daniel Marinado <daniel.marinado@gmail.com>
2022-03-11 17:42:23 -0300

    corrige route books count()

--- server/routes/book.js
+++ server/routes/book.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const { create, listAll, booksCount, removeSoft, read, update, list } = require("../controllers/book");
 
 router.post("/book", create);
+router.get("/books/total", booksCount);  
 router.get("/books/:count", listAll);
-router.get("/bookss/total", booksCount);  
 router.patch("/book/:slug", removeSoft);
 router.get("/book/:slug", read);
 router.put("/book/:slug", update);
